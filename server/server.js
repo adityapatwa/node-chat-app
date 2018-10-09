43212736a5158974ebcc0c9d7c7abb72f19a9343
Aditya Patwa <adityapatwa@live.com>
2018-10-09 16:55:52 -0400

    Now new user will be greeted with a message and other users will be notified of a new user

--- server/server.js
+++ server/server.js
@@ -13,6 +13,18 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (newMessage) => {
         console.log('New message from the client');
         console.log(newMessage);
@@ -20,7 +32,12 @@ io.on('connection', (socket) => {
             from: newMessage.from,
             text: newMessage.text,
             createdAt: new Date().getTime()
-        })
+        });
+        // socket.broadcast.emit('newMessage', {
+        //     from: newMessage.from,
+        //     text: newMessage.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
