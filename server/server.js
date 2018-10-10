f2a7027fa00a7e3f67860101be9e0f748dcd2b85
Aditya Patwa <adityapatwa@live.com>
2018-10-10 11:40:03 -0400

    Added a form to send messages and added a list to display them

--- server/server.js
+++ server/server.js
@@ -16,13 +16,13 @@ io.on('connection', (socket) => {
 
     socket.emit('newMessage', generateMessage('admin', 'Welcome to the chat app'));
 
-    socket.broadcast.emit('newMessage', generateMessage('from', 'New user joined'));
+    socket.broadcast.emit('newMessage', generateMessage('admin', 'New user joined'));
 
-    socket.on('createMessage', (newMessage) => {
+    socket.on('createMessage', (newMessage, callback) => {
         console.log('New message from the client');
         console.log(newMessage);
         io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
-
+        callback('This is from the server');
         // socket.broadcast.emit('newMessage', {
         //     from: newMessage.from,
         //     text: newMessage.text,
